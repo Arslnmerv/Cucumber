a9bb4b8dee2d1fd4a08345f4832cbf02e89e64cf
Arslnmrv <arslanmerve1619@gmail.com>
2021-11-18 15:01:49 +0300

    updated

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         plugin={"html:target\\cucumber-reports.html"},
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@editor"  ,
+        tags = "@gmibank"  ,
 
         dryRun =false
         // dryRun=true dedigimizde testi calistirmadan bize eksik adimi verir
---
+++ src/test/java/stepdefinitions/US3001_StepDefinitions.java
@@ -0,0 +1,44 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+import pages.US3001_Page;
+
+import static org.junit.Assert.*;
+
+public class US3001_StepDefinitions {
+
+    US3001_Page us3001_page = new US3001_Page();
+    @Then("gmibank giris butonuna tiklar")
+    public void gmibankGirisButonunaTiklar() {
+     us3001_page.ilkGirisButonu.click();
+    }
+
+    @Then("gmibank register butonuna tiklar")
+    public void gmibankRegisterButonunaTiklar() {
+        us3001_page.registerButonu.click();
+    }
+
+    @And("gmibank password kutusuna tiklar")
+    public void gmibankPasswordKutusunaTiklar() {
+        us3001_page.passwordKutusu.click();
+    }
+
+    @Given("{string} girer")
+    public void girer(String password) throws InterruptedException {
+        us3001_page.passwordKutusu.sendKeys(password);
+        Thread.sleep(2000);
+    }
+
+    @Then("gmibank password strength red gorunur")
+    public void gmibankPasswordStrengthRedGorunur() {
+       assertTrue(us3001_page.red.isDisplayed());
+    }
+
+    @Then("gmibank password strength orange gorunur")
+    public void gmibankPasswordStrengthOrangeGorunur() {
+        assertTrue(us3001_page.orange.isDisplayed());
+    }
+}
